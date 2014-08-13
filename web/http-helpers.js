cd0d30061add519a3a7488236816628987d77562
Daniel Streit <Daniel.Streit@gmail.com>
2014-08-13 14:30:09 -0700

    Determine static file directories

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -2,6 +2,12 @@ var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
 
+var staticFiles = {
+  '': 'index.html',
+  'styles.css': 'styles.css',
+  'loading.html': 'loading.html'
+};
+
 exports.headers = headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
@@ -13,6 +19,24 @@ exports.headers = headers = {
 exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+  var dir;
+  var statusCode = 200;
+  res.writeHead(statusCode, headers);
+
+  var stream = fs.createReadStream(getPath(asset));
+  stream.on('data', function(chunk) {
+    res.write(chunk);
+  });
+  stream.on('end', function() {
+    res.end();
+  });
+};
+
+var getPath = function(asset) {
+  if (staticFiles[asset]) {
+    return './web/public/' + staticFiles[asset];
+  }
+  return './archives/sites/' + asset;
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,30 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var headers = require('./http-helpers').headers;
+var serveAssets = require('./http-helpers').serveAssets;
+var postURL = require('./http-helpers').postURL;
 // require more modules/folders here!
 
+var handleGet = function(req, res) {
+  serveAssets(res, path.basename(req.url));
+};
+
+var handlePost = function(req, res) {
+  var site = '';
+  req.on('data', function(chunk) {
+    site += chunk;
+  });
+  req.on('end', function() {
+    // if site already archived, take to archived site
+    // else add site and take to loading
+  });
+};
+
+var actionMap = {
+  'GET': handleGet,
+  'POST': handlePost
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  actionMap[req.method](req, res);
 };
